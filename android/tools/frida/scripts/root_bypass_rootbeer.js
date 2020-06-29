656e55211ef4bcff9a9f41b133996c93d5ed718c
haxxinen <32751905+haxxinen@users.noreply.github.com>
2020-06-29 12:43:55 +0200

    Update root_bypass_rootbeer.js

--- android/tools/frida/scripts/root_bypass_rootbeer.js
+++ android/tools/frida/scripts/root_bypass_rootbeer.js
@@ -1,17 +1,64 @@
+Java.perform(function() {
+
+    try {
+
+        var RootBeer = Java.use("com.scottyab.rootbeer.RootBeer");
+        RootBeer.isRooted.overload().implementation = function() {
+            return false;
+        };
+
+        // native bypass
+        var RootBeer = Java.use("com.scottyab.rootbeer.RootBeerNative");
+        RootBeer.checkForRoot.overload('[Ljava.lang.Object;').implementation = function() {
+            return false;
+        };
+
+        // -----
+        // alternative implementations for obfuscated methods
+
+        // isRootedWithBusyBoxCheck
+        var RootBeer = Java.use("com.scottyab.rootbeer.d");
+        RootBeer.s.overload().implementation = function() {
+            return false;
+        };
+        /*
+         package com.scottyab.rootbeer;
+         public class d {
+            public boolean s() {
+               return o() || k() || b("su") || b("busybox") || d() || g() || q() || j() || h() || e();
+            }
+         }
+        */
+
+        // isRooted
+        RootBeer.t.overload().implementation = function() {
+            return false;
+        };
+        /*
+         package com.scottyab.rootbeer;
+         public class d {
+            public boolean t() {
+               return o() || k() || b("su") || d() || g() || q() || j() || h() || e();
+            }
+         }
+        */
+    } catch (err) {
+        // do nothing
+    }
+
+});
+
+// https://github.com/scottyab/rootbeer/blob/master/rootbeerlib/src/main/java/com/scottyab/rootbeer/RootBeer.java
+
 // app: github.com/scottyab/rootbeer
 // lib: github.com/scottyab/rootbeer/tree/master/rootbeerlib
 // apk: https://play.google.com/store/apps/details?id=com.scottyab.rootbeer.sample
 // blog: https://medium.com/@scottyab/detecting-root-on-android-97803474f694
+// tools: JadxGui / objection
 
 // custom app
 // ./smali_classes6/com/company/ui/activities/SplashScreenActivity.smali:
 // invoke-virtual {v0}, Lcom/scottyab/rootbeer/RootBeer;->isRooted()Z
 
-Java.perform(function () {
-  var RootBeer = Java.use("com.scottyab.rootbeer.RootBeer");
-  RootBeer.isRooted.overload().implementation = function () { 
-    console.log('com.scottyab.rootbeer.RootBeer.isRooted()->false');
-    return false;
-  };
-});
-
+// # objection --gadget "com.app.target" explore --startup-command "android hooking list classes"
+// # objection --gadget "com.app.target" explore --startup-command "android hooking list class_methods com.scottyab.rootbeer.RootBeer"
